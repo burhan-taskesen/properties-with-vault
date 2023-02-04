5288cbb4a37377bfc6f227dc49c2f4f04b5c966d
burhan-taskesen <taskesenburhan@gmail.com>
2023-02-04 16:23:50 +0300

    Initial commit

---
+++ src/main/java/com/example/propertieswithvault/PropertiesWithVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.propertieswithvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesWithVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertiesWithVaultApplication.class, args);
+    }
+
+}
